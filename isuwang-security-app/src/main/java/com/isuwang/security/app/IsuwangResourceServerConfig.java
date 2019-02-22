a454675ebf2e4587b0bdf8856da6fa1f595efeff
liurongjie <liurongjie@iplas.com.cn>
2019-02-22 20:36:29 +0800

    完成app登录校验成功Oauth2返回token

--- isuwang-security-app/src/main/java/com/isuwang/security/app/IsuwangResourceServerConfig.java
+++ isuwang-security-app/src/main/java/com/isuwang/security/app/IsuwangResourceServerConfig.java
@@ -1,10 +1,77 @@
 package com.isuwang.security.app;
 
+import com.isuwang.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import com.isuwang.security.core.properties.SecurityConstants;
+import com.isuwang.security.core.properties.SecurityProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 @Configuration
 @EnableResourceServer
-public class IsuwangResourceServerConfig {
+public class IsuwangResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    /**
+     * 注入security配置
+     */
+    @Autowired
+    private SecurityProperties securityProperties;
+
+    /**
+     * 注入校验成功处理器
+     */
+    @Autowired
+    private AuthenticationSuccessHandler isuwangAuthenticationSuccessHandler;
+
+    /**
+     * 注入校验失败处理器
+     */
+    @Autowired
+    private AuthenticationFailureHandler isuwangAuthenticationFailureHandler;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    /**
+     * 第三方登录配置
+     */
+    @Autowired
+    private SpringSocialConfigurer socialSecurityConfigurer;
+
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+
+//        applyPasswordAuthenticationConfig(http);
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_SIGN_IN_PROCESSING_URL_FORM) //自定义登录请求拦截URL
+                .successHandler(isuwangAuthenticationSuccessHandler)
+                .failureHandler(isuwangAuthenticationFailureHandler);
+
+        http //.apply(validateCodeSecurityConfig)
+//                .and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .apply(socialSecurityConfigurer)
+                .and()
+                .authorizeRequests()  // 配置验证配置
+                    .antMatchers(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_SIGN_IN_PROCESSING_URL_MOBILE,
+                        securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX + "/*",
+                        securityProperties.getBrowser().getSignUpUrl())
+                .permitAll()
+                .anyRequest()  // 所有的请求都要通过验证
+                .authenticated()
+                .and()
+                .csrf().disable();
+    }
 }
--- isuwang-security-app/src/main/java/com/isuwang/security/app/authentication/IsuwangAuthenticationSuccessHandler.java
+++ isuwang-security-app/src/main/java/com/isuwang/security/app/authentication/IsuwangAuthenticationSuccessHandler.java
@@ -3,10 +3,19 @@ package com.isuwang.security.app.authentication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.isuwang.security.core.properties.LoginType;
 import com.isuwang.security.core.properties.SecurityProperties;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.client.token.OAuth2AccessTokenSupport;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -31,21 +40,70 @@ public class IsuwangAuthenticationSuccessHandler extends SavedRequestAwareAuthen
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
 
         logger.info("登录成功");
+        String header = request.getHeader("Authorization");
+        if (header == null || !header.toLowerCase().startsWith("basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头无client信息");
+        }
+
+        String[] tokens = extractAndDecodeHeader(header, request);
+        assert tokens.length == 2;
+
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+        if(clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId对应的匹配信息不存在：" + clientId);
+        }else if(!StringUtils.equals(clientDetails.getClientSecret(), clientSecret)) {
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配：" + clientId);
+        }
+
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "customer");
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
         /*
          * 登录返回类型是JSON，则以json返回,否则调用父类方法跳转
          */
-        if(LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())){
-            response.setContentType("application/json;charset=UTF-8");
-            response.getWriter().write(objectMapper.writeValueAsString(authentication));
-        }else{
-            super.onAuthenticationSuccess(request, response, authentication);
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(token));
+
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request)
+            throws IOException {
+
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        }
+        catch (IllegalArgumentException e) {
+            throw new BadCredentialsException(
+                    "Failed to decode basic authentication token");
         }
 
+        String token = new String(decoded, "UTF-8");
 
+        int delim = token.indexOf(":");
+
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        }
+        return new String[] { token.substring(0, delim), token.substring(delim + 1) };
     }
 }
